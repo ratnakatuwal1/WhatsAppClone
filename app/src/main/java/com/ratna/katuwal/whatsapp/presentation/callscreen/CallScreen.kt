b4f2ebd9cbf7e67fda374f49385142d7e2914750
Ratna Katuwal <parbesh456@gmail.com>
2026-03-15 22:02:50 +0545

    Initial Commit with the update

---
+++ app/src/main/java/com/ratna/katuwal/whatsapp/presentation/callscreen/CallScreen.kt
@@ -0,0 +1,11 @@
+package com.ratna.katuwal.whatsapp.presentation.callscreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview(showSystemUi = true)
+fun CallScreen(modifier: Modifier = Modifier) {
+
+}
